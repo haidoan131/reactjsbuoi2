aa70c25cc999790141df5674d49e1513e8c18156
haidoan131 <zzzdoandu@gmail.com>
2024-07-22 09:58:51 +0700

    first commit

---
+++ src/components/BaiTapForm.js
@@ -0,0 +1,20 @@
+import React ,{useState}from 'react'
+
+export default function BaiTap() {
+
+    const[result,setResult]=useState(0)
+    const[a,setA]=useState(0)
+    const[b,setB]=useState(0)
+  return (
+    <div>
+      
+        <h1>kết quả:{result}</h1>
+        <input type="text" onChange={(e)=>setA(e.target.value*1)}/><br/>
+        <input type="text"  onChange={(e)=>setB(e.target.value*1)}/><br/>
+        <button onClick={()=>setResult(a+b)}>+</button>
+        <button onClick={()=>setResult(a-b)}>  -</button>
+        <button onClick={()=>setResult(a*b)}>*</button>
+        <button onClick={()=>setResult((a/b).toFixed(2))}>/</button>
+    </div>
+  )
+}

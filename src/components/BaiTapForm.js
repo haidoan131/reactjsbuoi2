3664b32cd3e7b4ab12eb506b475d986f97e15f0b
haidoan131 <zzzdoandu@gmail.com>
2024-07-22 10:07:39 +0700

    update lan 2

--- src/components/BaiTapForm.js
+++ src/components/BaiTapForm.js
@@ -7,7 +7,7 @@ export default function BaiTap() {
     const[b,setB]=useState(0)
   return (
     <div>
-      
+        <h1>tesstttttttttttttttt</h1>
         <h1>kết quả:{result}</h1>
         <input type="text" onChange={(e)=>setA(e.target.value*1)}/><br/>
         <input type="text"  onChange={(e)=>setB(e.target.value*1)}/><br/>
